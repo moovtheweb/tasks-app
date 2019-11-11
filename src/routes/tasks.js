a51ee3e0d8d729c07201e5ad03e828c3370e3ca2
moovtheweb <moovtheweb@gmail.com>
2019-11-11 08:09:48 +0000

    added Patch, Delete routes for tasks and users

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -31,10 +31,49 @@ taskRouter.post("/tasks", async (req,res)=>{
     try{
         await task.save()
         console.log("saved" + task)
+        return res.status(201).send(task)
     }catch(error){
             console.log("error"+error)
+            return res.status(400).send()
     }
     res.send(task)
 })
 
+taskRouter.patch("/tasks/:id", async (req,res)=>{
+    console.log(req.params)
+    _id=req.params.id
+    try{
+        const task = await Task.findByIdAndUpdate(_id,req.body,{new:true,runValidators:true})
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        return res.status(500).send()
+    }
+})
+
+taskRouter.delete("/tasks/:id", async (req,res)=>{
+    console.log(req.params)
+    _id=req.params.id
+    try{
+        const task = await Task.findByIdAndDelete(_id,req.body)
+        if(!task){
+            return res.status(404).send("task not found")
+        }
+        res.send(task)
+    } catch (e) {
+        return res.status(500).send()
+    }
+})
+
 module.exports = taskRouter
+
+//below code block can check if the keys sent in the request body shoudl be allowed to update 
+//the document in DB. this is not scalable. there should be a better way and also should be
+//done at a central place. Hence this code is not incorporated and is only kept for refernece
+// const updates = Object.keys(req.body)
+// const allowedKeys = ['name', 'email','age']
+// const validOperation = updates.every((update)=>{
+//     return allowedKeys.includes(update)})
+//if validOperation is false don'r let the update go through
--- src/routes/users.js
+++ src/routes/users.js
@@ -20,7 +20,7 @@ userRouter.get("/users/:id", async (req,res)=> {
     console.log(req.params)
     const _id = req.params.id
     try{
-        user = await User.findById(_id)
+        const user = await User.findById(_id)
         if(!user){
              return res.status(404).send()
          }
@@ -36,12 +36,42 @@ userRouter.post("/users", async (req,res)=>{
     try{
         await user.save()
         console.log("saved" + user)
-        return res.status(200).send("user created")
+        return res.status(201).send(user)
     } catch(error){
         console.log("error"+error)
-        return res.status(500).send()
+        return res.status(400).send()
     }
     res.send(user)
+})
+
+userRouter.patch("/users/:id", async (req,res)=> {
+    console.log(req.params)
+    const _id = req.params.id
+    try{
+        const user = await User.findByIdAndUpdate(_id,req.body,{new:true, runvalidators:true, })
+        if(!user){
+             return res.status(404).send()
+         }
+         res.status(200).send(user)
+     } catch(e){
+        console.log('error in fetching users')
+        return res.status(500).send(e)
+    }
+})
+
+userRouter.delete("/users/:id", async (req,res)=> {
+    console.log(req.params)
+    const _id = req.params.id
+    try{
+        const user = await User.findByIdAndUpdate(_id)
+        if(!user){
+             return res.status(404).send()
+         }
+         res.status(200).send(user)
+     } catch(e){
+        console.log('error in fetching users')
+        return res.status(500).send(e)
+    }
 })
 
 module.exports = userRouter
